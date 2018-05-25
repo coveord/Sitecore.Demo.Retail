f4588aa1125196b327cb69c8d9c4556deb08f874
Jean-François L'Heureux <spot_web@hotmail.com>
2018-05-25 07:20:51 -0400

    Index non-product pages

---
+++ src/Foundation/Commerce.CoveoCommerceIndexing/code/Processors/FetchPageContent/PostProcessing/CleanHtmlTags.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Coveo.Framework.CNL;
+using Coveo.Framework.Log;
+using Coveo.Framework.Utils;
+using Coveo.SearchProvider.Processors.FetchPageContent.PostProcessing;
+using Coveo.SearchProvider.Utils;
+using Sitecore.Foundation.Commerce.CoveoCommerceIndexing.Utils;
+
+namespace Sitecore.Foundation.Commerce.CoveoCommerceIndexing.Processors.FetchPageContent.PostProcessing {
+    public class CleanHtmlTags : IFetchPageContentHtmlPostProcessingProcessor
+    {
+        private static bool s_HasLoggedErrorOnce;
+
+        private static readonly ILogger s_Logger = CoveoLogManager.GetLogger(MethodBase.GetCurrentMethod()
+                                                                                       .DeclaringType);
+
+        private readonly IHtmlCleaner m_HtmlCleaner;
+
+        /// <summary>
+        /// Gets or sets the text inside the HTML start tag where to start removing markup.
+        /// </summary>
+        public string StartTagContent { get; set; }
+
+        /// <summary>
+        /// Gets or sets the text inside the HTML end tag where to end removing markup.
+        /// </summary>
+        public string EndTagContent { get; set; }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="CleanHtmlTags"/>.
+        /// </summary>
+        public CleanHtmlTags()
+            : this(new HtmlTagsCleaner())
+        { }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="CleanHtmlTags"/>.
+        /// </summary>
+        /// <param name="p_HtmlCleaner">The <see cref="IHtmlCleaner"/> used to clean the HTML.</param>
+        public CleanHtmlTags(IHtmlCleaner p_HtmlCleaner)
+        {
+            Precondition.NotNull(p_HtmlCleaner, () => () => p_HtmlCleaner);
+
+            m_HtmlCleaner = p_HtmlCleaner;
+        }
+
+        /// <inheritDoc />
+        public void Process(FetchPageContentHtmlPostProcessingArgs p_Args)
+        {
+            Precondition.NotNull(p_Args, () => () => p_Args);
+
+            if (ValidateParameters()) {
+                p_Args.HtmlContent = m_HtmlCleaner.CleanHtmlContent(p_Args.HtmlContent, StartTagContent, EndTagContent);
+            }
+        }
+
+        private bool ValidateParameters()
+        {
+            List<string> invalidParameters = new List<string>();
+
+            if (String.IsNullOrWhiteSpace(StartTagContent)) {
+                invalidParameters.Add("StartTagContent");
+            }
+
+            if (String.IsNullOrWhiteSpace(EndTagContent)) {
+                invalidParameters.Add("EndTagContent");
+            }
+
+            if (StartTagContent.EqualsIgnoreCase(EndTagContent)) {
+                invalidParameters.Add("StartTagContent");
+                invalidParameters.Add("EndTagContent");
+                s_Logger.Warn("The StartTagContent and EndTagContent values are the same. Please use different values.");
+            }
+
+            if (invalidParameters.Any()) {
+                string errorMessage = String.Format("The \"{0}\" processor will not be executed because it is missing the following parameters: {1}.",
+                                                    typeof(CleanHtmlTags).FullName,
+                                                    String.Join(", ", invalidParameters));
+                LogWarningOnTheFirstOccurrence(errorMessage);
+            }
+
+            return !invalidParameters.Any();
+        }
+
+        private static void LogWarningOnTheFirstOccurrence(string p_Message)
+        {
+            if (!s_HasLoggedErrorOnce) {
+                s_HasLoggedErrorOnce = true;
+                s_Logger.Warn(p_Message);
+            } else {
+                s_Logger.Debug(p_Message);
+            }
+        }
+    }
+}
---
+++ src/Foundation/Commerce.CoveoCommerceIndexing/code/Utils/HtmlTagsCleaner.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Coveo.Framework.CNL;
+using Coveo.SearchProvider.Utils;
+
+namespace Sitecore.Foundation.Commerce.CoveoCommerceIndexing.Utils {
+    public class HtmlTagsCleaner : IHtmlCleaner
+    {
+        /// <inheritDoc/>
+        public string CleanHtmlContent(string p_HtmlContent,
+                                       string p_StartTagContent,
+                                       string p_EndTagContent)
+        {
+            Precondition.NotEmpty(p_HtmlContent, () => () => p_HtmlContent);
+            Precondition.NotEmpty(p_StartTagContent, () => () => p_StartTagContent);
+            Precondition.NotEmpty(p_EndTagContent, () => () => p_EndTagContent);
+
+            string htmlContent = p_HtmlContent;
+            string startTagContent = Regex.Escape(p_StartTagContent);
+            string endTagContent = Regex.Escape(p_EndTagContent);
+            Regex startTagRegex = new Regex(@"<\s*" + startTagContent + @"\s*>", RegexOptions.Singleline | RegexOptions.Compiled);
+            Regex endTagRegex = new Regex(@"<\s*" + endTagContent + @"\s*>", RegexOptions.Singleline | RegexOptions.Compiled);
+
+            Match startTagMatch = startTagRegex.Match(htmlContent);
+            Match endTagMatch = endTagRegex.Match(htmlContent);
+
+            while (StartTagIsBeforeEndTag(startTagMatch, endTagMatch)) {
+                int firstPartLength = startTagMatch.Index;
+                startTagMatch = startTagMatch.NextMatch();
+                while (EndTagIsAfterNextStartTag(startTagMatch, endTagMatch)) {
+                    startTagMatch = startTagMatch.NextMatch();
+                    endTagMatch = endTagMatch.NextMatch();
+                }
+                int secondPartStartIndex = endTagMatch.Index + endTagMatch.Length;
+                int secondPartLength = htmlContent.Length - secondPartStartIndex;
+                htmlContent = htmlContent.Substring(0, firstPartLength) + htmlContent.Substring(secondPartStartIndex, secondPartLength);
+                startTagMatch = startTagRegex.Match(htmlContent);
+                endTagMatch = endTagRegex.Match(htmlContent);
+            }
+
+            return htmlContent;
+        }
+
+        /// <inheritDoc/>
+        public bool ValidateHtmlCleaningDelimiters(string p_StartDelimiterText,
+                                                   string p_EndDelimiterText)
+        {
+            return IsValidDelimiterText(p_StartDelimiterText) &&
+                   IsValidDelimiterText(p_EndDelimiterText) &&
+                   (p_StartDelimiterText != p_EndDelimiterText);
+        }
+
+        private bool IsValidDelimiterText(string p_DelimiterText)
+        {
+            return !String.IsNullOrWhiteSpace(p_DelimiterText) &&
+                   p_DelimiterText.All(IsValidDelimiterCharacter);
+        }
+
+        private bool IsValidDelimiterCharacter(char p_CommentCharacter)
+        {
+            return p_CommentCharacter != '<' &&
+                   p_CommentCharacter != '>';
+        }
+
+        private bool StartTagIsBeforeEndTag(Match p_StartTagMatch,
+                                            Match p_EndTagMatch)
+        {
+            return p_StartTagMatch.Success &&
+                   p_EndTagMatch.Success &&
+                   p_StartTagMatch.Index <= p_EndTagMatch.Index;
+        }
+
+        private bool EndTagIsAfterNextStartTag(Match p_StartTagMatch,
+                                               Match p_EndTagMatch)
+        {
+            return p_EndTagMatch.Index > p_StartTagMatch.Index &&
+                   p_StartTagMatch.Success &&
+                   p_EndTagMatch.Success;
+        }
+    }
+}
